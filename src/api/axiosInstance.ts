e24a9f8187fbae9e681b5d7e5b5e4f36d379be2f
ehdgus02 <ehdgus02kk@naver.com>
2026-05-15 15:49:38 +0900

    SignInPage 작성 + 리팩토링

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -9,3 +9,10 @@ const api = axios.create({
 });
 
 export default api;
+
+// axios : 라이브러리 O  (Typescript 지원, 별도 보조 라이브러리 X)
+// - 도착된 결과의 상태 코드를 읽기 때문에 2xx번 응답 제외 모두 에러
+// - 에러 처리를 할 때 catch문에서 통합하여 에러 처리 가능
+// - 자동 JSON 변환을 지원하기 때문에 별도로 파싱 필요 없음
+// - intercept 라는 기능을 제공하여, Express의 미들웨어처럼
+//   전송 전에 신분증 내용을 집어넣는 등의 기능을 한 방에 처리 가능
---
+++ src/components/common/select/Select.tsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+const Select = styled.select<{ $hasError?: boolean }>`
+    width: 100%;
+    padding: 12px 16px;
+    background-color: ${props => props.theme.color.background.default};
+    border: 1px solid
+        ${props => (props.$hasError ? props.theme.color.error : props.theme.color.divider)};
+    border-radius: 8px;
+    font-size: 15px;
+    color: ${props => props.theme.color.text.default};
+    transition: all 0.5s;
+
+    &::placeholder {
+        color: ${props => props.theme.color.text.disabled};
+    }
+
+    &:focus {
+        border-color: ${props =>
+            props.$hasError ? props.theme.color.error : props.theme.color.primary};
+    }
+`;
+
+export default Select;
