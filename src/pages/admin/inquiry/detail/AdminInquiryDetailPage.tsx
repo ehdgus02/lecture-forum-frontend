d65fe6d54596e762370f14ea8ebe05e115295352
ehdgus02 <ehdgus02kk@naver.com>
2026-06-12 14:46:59 +0900

    AdminInquiryAnswerForm 1차 작성 완료

--- src/pages/admin/inquiry/detail/AdminInquiryDetailPage.tsx
+++ src/pages/admin/inquiry/detail/AdminInquiryDetailPage.tsx
@@ -11,8 +11,10 @@ import {
     LoadingText,
     PostContainer,
 } from "../../../../components/post/post.style.tsx";
-import { AdminButtonGroup } from "../../../../components/admin/admin.style.tsx";
+import { AdminButtonGroup, AnswerSection } from "../../../../components/admin/admin.style.tsx";
 import Button from "../../../../components/common/button/Button.tsx";
+import AdminInquiryAnswerForm from "../../../../components/inquiry/AdminInquiryAnswerForm.tsx";
+import AdminInquiryAnswerBox from "../../../../components/inquiry/AdminInquiryAnswerBox.tsx";
 
 function AdminInquiryDetailPage() {
     const navigate = useNavigate();
@@ -62,6 +64,20 @@ function AdminInquiryDetailPage() {
                 </DetailHeader>
 
                 <DetailContent>{inquiry.content}</DetailContent>
+
+                <hr />
+
+                {/*
+                     만약에, 답변이 아직 달리지 않았다면 Textarea를 띄워서 답변을 달 수 있도록 할 것이고
+                            답변이 이미 달렸다면 답변 내용이 출력될 수 있도록 함
+                */}
+                <AnswerSection>
+                    {inquiry.answer ? (
+                        <AdminInquiryAnswerBox />
+                    ) : (
+                        <AdminInquiryAnswerForm inquiryId={inquiryId} />
+                    )}
+                </AnswerSection>
 
                 <AdminButtonGroup style={{ marginTop: "40px" }}>
                     <Button color={"secondary"} variant={"contained"} onClick={() => navigate(-1)}>
